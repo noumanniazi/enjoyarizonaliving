a37069bf2e776d99d0aea606242f13a20aa8afe2
nouman <nouman.aamir@ofload.com>
2023-08-24 12:49:09 -0400

    Aug-24-2023 upload website

---
+++ js/google-map.js
@@ -0,0 +1,7 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(55.508742,12.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
